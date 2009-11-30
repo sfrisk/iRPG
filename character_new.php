4abe0c0aeb9cf0350c549a2aa4611a557aa57c9e
Sarah Frisk <sarah@sarahfrisk.com>
2009-11-30 09:51:24 -0500

    took out javascript from html, added some functionality for selecting race

--- character_new.php
+++ character_new.php
@@ -26,16 +26,16 @@ include('header.php');
 <form action="" method="post">
 		<p>
 			<label for="name">Name:</label>
-			<input type="text" name="name" / >
+			<input type="text" id="name" / >
 			
 			<label for="level">Level:</label>
-			<select name="level">
+			<select id="level">
 			<option value="1" selected="selected">1</option>
 			<!-- Will put more options in at later date, for now only dealing with level 1 characters-->
 			</select>
 			
 			<label for="class">Class:</label>
-			<select name="class">
+			<select id="class">
 				<option value="<?=NO_CLASS?>"></option>
 				<option value="<?=BARBARIAN?>">Barbarian</option>
 				<option value="<?=BARD?>">Bard</option>
@@ -51,22 +51,22 @@ include('header.php');
 			</select>
 
 			<label for="race">Race:</label>
-			<select name="race">
-				<option value="<?=NO_RACE?>"></option>
-				<option value="<?=HUMAN?>">Human</option>
-				<option value="<?=DWARF?>">Dwarf</option>
-				<option value="<?=ELF?>">Elf</option>
-				<option value="<?=GNOME?>">Gnome</option>
-				<option value="<?=HALF-ELF?>">Half-Elf</option>
-				<option value="<?=HALF-ORC?>">Half-Orc</option>
-				<option value="<?=HALFLING?>">Halfling</option>
+			<select id="race">
+				<option value="NO_RACE"></option>
+				<option value="HUMAN">Human</option>
+				<option value="DWARF">Dwarf</option>
+				<option value="ELF">Elf</option>
+				<option value="GNOME">Gnome</option>
+				<option value="HALF-ELF">Half-Elf</option>
+				<option value="HALF-ORC">Half-Orc</option>
+				<option value="HALFLING">Halfling</option>
 			</select>
 			
 		</p>
 		<p>
 
 			<label for="alignment">Alignment:</label>
-			<select name="alignment">
+			<select id="alignment">
 				<option value="<?=NO_ALIGNMENT?>"> </option>
 				<option value="<?=LAWFUL_GOOD?>">Lawful Good</option>
 				<option value="<?=NEUTRAL_GOOD?>">Neutral Good</option>
@@ -80,7 +80,7 @@ include('header.php');
 			</select>
 
 			<label for="diety">Diety:</label>
-			<select name="diety">
+			<select id="diety">
 				<option value="<?=NO_DIETY?>">None</option>
 				<option value="<?=BOCCOB?>">Boccob</option>
 				<option value="<?=CORELLON_LARETHIAN?>">Corellon Larethian</option>
@@ -105,17 +105,17 @@ include('header.php');
 			</select>
 
 			<label for"size">Size</label>
-			<select name="size">
-				<option value="<?=NO_SIZE?>"></option>
-				<option value="<?=SMALL?>">Small</option>
-				<option value="<?=MEDIUM?>">Medium</option>
+			<select id="size">
+				<option value="NO_SIZE"></option>
+				<option value="SMALL">Small</option>
+				<option value="MEDIUM">Medium</option>
 			</select>
 
 			<label for="age">Age:</label>
-			<input type="text" name="age" size="3" / >
+			<input type="text" id="age" size="3" / >
 
 			<label for="gender">Gender:</label>
-			<select name="gender">
+			<select id="gender">
 				<option value="<?=NO_GENDER?>"></option>
 				<option value="<?=FEMALE?>">Female</option>
 				<option value="<?=MALE?>">Male</option>
@@ -146,61 +146,61 @@ include('header.php');
 		</tr>
 			<tr>
 				<td>AC <br /><span class="label">Armor Class</span></td>
-				<td><input type="text" name="total" size="3" / ></td>
+				<td><input type="text" id="total" size="3" / ></td>
 				<td>= 10 +</td>
-				<td><input disabled="disabled" type="text" name="ARMOR_BONUS" size="3" / ></td>
+				<td><input disabled="disabled" type="text" id="ARMOR_BONUS" size="3" / ></td>
 				<td> + </td>
-				<td><input disabled="disabled" type="text" name="SHIELD_BONUS" size="3" / ></td>
+				<td><input disabled="disabled" type="text" id="SHIELD_BONUS" size="3" / ></td>
 				<td> + </td>
-				<td><input disabled="disabled" type="text" name="DEX_MOD" size="3" / ></td>
+				<td><input disabled="disabled" type="text" id="DEX_MOD1" size="3" / ></td>
 				<td> + </td>
-				<td><input disabled="disabled" type="text" name="SIZE_MOD" size="3" / ></td>
+				<td><input disabled="disabled" type="text" id="SIZE_MOD0" size="3" / ></td>
 				<td> + </td>
-				<td><input disabled="disabled" type="text" name="NATURAL_ARMOR" size="3" / ></td>
+				<td><input disabled="disabled" type="text" id="NATURAL_ARMOR" size="3" / ></td>
 				<td> + </td>
-				<td><input disabled="disabled" type="text" name="DEFLECTION_MOD" size="3" / ></td>
+				<td><input disabled="disabled" type="text" id="DEFLECTION_MOD" size="3" / ></td>
 				<td> + </td>
-				<td><input disabled="disabled" type="text" name="MISC_MOD_ARMOR_CLASS" size="3" / ></td>
+				<td><input disabled="disabled" type="text" id="MISC_MOD_ARMOR_CLASS" size="3" / ></td>
 			</tr>
 		</table>
 		
 		<table cellspacing="0" class="character_table2">
 		<tr>
 			<td>Touch<br /><span class="label">Armor Class</span></td>
-			<td><input disabled="disabled" type="text" name="TOUCH" size="3" / ></td>
+			<td><input disabled="disabled" type="text" id="TOUCH" size="3" / ></td>
 		</tr>
 		</table>
 		
 		<table cellspacing="0" class="character_table2">
 		<tr>
 			<td>Flat-Footed<br /><span class="label">Armor Class</span></td>
-			<td><input disabled="disabled" type="text" name="FLAT_FOOTED" size="3" / ></td>
+			<td><input disabled="disabled" type="text" id="FLAT_FOOTED" size="3" / ></td>
 		</tr>
 		</table>
 		
 		<table cellspacing="0" class="character_table2">
 		<tr>
 			<td>HP<br /><span class="label">Hit Points</span></td>
-			<td><input disabled="disabled" type="text" name="HIT_POINTS" size="3" / ></td>
+			<td><input disabled="disabled" type="text" id="HIT_POINTS" size="3" / ></td>
 		</tr>
 		</table>	
 		<table cellspacing="0" class="character_table2">
 		<tr>
 			<td>Speed</td>
-			<td><input disabled="disabled" type="text" name="SPEED" size="3" / ></td>
+			<td><input disabled="disabled" type="text" id="SPEED" size="3" / ></td>
 		</tr>
 		</table>
 
 		<table cellspacing="0" class="character_table2">
 		<tr>
 			<td>Base Attack Bonus</td>
-			<td><input disabled="disabled" type="text" name="BASE_ATTACK_BONUS" size="3" / ></td>
+			<td><input disabled="disabled" type="text" id="BASE_ATTACK_BONUS" size="3" / ></td>
 		</tr>
 		</table>
 		<table cellspacing="0" class="character_table2">
 		<tr>
 			<td>Spell Resistance</td>
-			<td><input disabled="disabled" type="text" name="SPELL_RESISTANCE" size="3" / ></td>
+			<td><input disabled="disabled" type="text" id="SPELL_RESISTANCE" size="3" / ></td>
 		</tr>
 		</table>
 		
@@ -220,15 +220,15 @@ include('header.php');
 		</tr>
 			<tr>
 				<td>Grapple<br /><span class="label">Modifier</span></td>
-				<td><input disabled="disabled" type="text" name="TOTA_GRAPPLE" size="3" / ></td>
+				<td><input disabled="disabled" type="text" id="TOTAL_GRAPPLE" size="3" / ></td>
 				<td> = </td>
-				<td><input disabled="disabled" type="text" name="BASE_ATTACK_BONUS" size="3" / ></td>
+				<td><input disabled="disabled" type="text" id="BASE_ATTACK_BONUS" size="3" / ></td>
 				<td> + </td>
-				<td><input disabled="disabled" type="text" name="STR_MOD" size="3" / ></td>
+				<td><input disabled="disabled" type="text" id="STR_MOD1" size="3" / ></td>
 				<td> + </td>
-				<td><input disabled="disabled" type="text" name="SIZE_MOD" size="3" / ></td>
+				<td><input disabled="disabled" type="text" id="SIZE_MOD1" size="3" / ></td>
 				<td> + </td>
-				<td><input disabled="disabled" type="text" name="MISC_MOD_GRAPPLE" size="3" / ></td>
+				<td><input disabled="disabled" type="text" id="MISC_MOD_GRAPPLE" size="3" / ></td>
 			</tr>
 		</table>
 
@@ -243,11 +243,11 @@ include('header.php');
 			<tr>
 			<tr>
 				<td><br />Initiative<br /><span class="label">Modifier</span></td>
-				<td><input disabled="disabled" type="text" name="TOTAL_INITIATIVE" size="3" / ></td>
+				<td><input disabled="disabled" type="text" id="TOTAL_INITIATIVE" size="3" / ></td>
 				<td>&nbsp;=&nbsp;</td>
-				<td><input disabled="disabled" type="text" name="DEX_MOD" size="3" / ></td>
+				<td><input disabled="disabled" type="text" id="DEX_MOD2" size="3" / ></td>
 				<td>&nbsp;+&nbsp;</td>
-				<td><input disabled="disabled" type="text" name="MISC_MOD" size="3" / ></td>
+				<td><input disabled="disabled" type="text" id="MISC_MOD" size="3" / ></td>
 			</tr>
 			</table>
 		
@@ -265,49 +265,49 @@ include('header.php');
 			</tr>
 			<tr>
 				<td>STR<br /><span class="label">Strength</span></td>
-				<td><input type="text" id="STR" size="3" value="0" onChange="setMod(this.value, this.id);" /></td>
-				<td><input disabled="disabled" type="text" id="STR_MOD" size="3" /></td
+				<td><input type="text" id="STR" size="3" value="0" /></td>
+				<td><input disabled="disabled" type="text" id="STR_MOD0" size="3" /></td
 				<td><input disabled="disabled" type="text" id="STR_SCORE_TEMP" size="3" /></td
 				<td><input disabled="disabled" type="text" id="STR_MOD_TEMP" size="3" /></td
 			</tr>
 			<tr>
 				<td>DEX<br /><span class="label">Dexterity</span></td>
-				<td><input type="text" id="DEX" size="3" value="0" onChange="setMod(this.value, this.id);" /></td>
-				<td><input disabled="disabled" type="text" id="DEX_MOD" size="3"  /></td>
+				<td><input type="text" id="DEX" size="3" value="0" /></td>
+				<td><input disabled="disabled" type="text" id="DEX_MOD0" size="3"  /></td>
 				<td><input disabled="disabled" type="text" id="DEX_SCORE_TEMP" size="3" /></td
 				<td><input disabled="disabled" type="text" id="DEX_MOD_TEMP" size="3" /></td
 			</tr>
 			<tr>
 				<td>CON<br /><span class="label">Constitution</span></td>
-				<td><input type="text" id="CON" size="3" value="0" onChange="setMod(this.value, this.id);" /></td>
-				<td><input disabled="disabled" type="text" id="CON_MOD" size="3" /></td>
+				<td><input type="text" id="CON" size="3" value="0" /></td>
+				<td><input disabled="disabled" type="text" id="CON_MOD0" size="3" /></td>
 				<td><input disabled="disabled" type="text" id="CON_SCORE_TEMP" size="3" /></td
 				<td><input disabled="disabled" type="text" id="CON_MOD_TEMP" size="3" /></td
 			</tr>
 			<tr>
 				<td>INT<br /><span class="label">Intelligence</span></td>
-				<td><input type="text" id="INT" size="3" value="0" onChange="setMod(this.value, this.id);" /></td>
-				<td><input disabled="disabled" type="text" id="INT_MOD" size="3" /></td>
+				<td><input type="text" id="INT" size="3" value="0" /></td>
+				<td><input disabled="disabled" type="text" id="INT_MOD0" size="3" /></td>
 				<td><input disabled="disabled" type="text" id="INT_SCORE_TEMP" size="3" /></td
 				<td><input disabled="disabled" type="text" id="INT_MOD_TEMP" size="3" /></td
 			</tr>
 			<tr>
 				<td>WIS<br /><span class="label">Wisdom</span></td>
-				<td><input type="text" id="WIS" size="3" value="0" onChange="setMod(this.value, this.id);" /></td>
-				<td><input disabled="disabled" type="text" id="WIS_MOD" size="3" /></td>
+				<td><input type="text" id="WIS" size="3" value="0" /></td>
+				<td><input disabled="disabled" type="text" id="WIS_MOD0" size="3" /></td>
 				<td><input disabled="disabled" type="text" id="WIS_SCORE_TEMP" size="3" /></td
 				<td><input disabled="disabled" type="text" id="WIS_MOD_TEMP" size="3" /></td
 			</tr>
 			<tr>
 				<td>CHA<br /><span class="label">Charisma</span></td>
-				<td><input type="text" id="CHA" size="3" onChange="setMod(this.value, this.id);" /></td>
-				<td><input disabled="disabled" type="text" id="CHA_MOD" size="3"  /></td>
+				<td><input type="text" id="CHA" size="3" value="0"  /></td>
+				<td><input disabled="disabled" type="text" id="CHA_MOD0" size="3"  /></td>
 				<td><input disabled="disabled" type="text" id="CHA_SCORE_TEMP" size="3" /></td
 				<td><input disabled="disabled" type="text" id="CHA_MOD_TEMP" size="3" /></td
 			</tr>
 		</table>
 		<p>
-			<input type="submit" name="submit" value = "Create Character">(currently does nothing)
+			<input type="submit" id="submit" value = "Create Character">(currently does nothing)
 		</p>
 
 		
--- js/character_calculations.js
+++ js/character_calculations.js
@@ -1,8 +1,88 @@
+window.onload = setup;
+
+var numericExpression = /^[0-9]+$/;
+
+function setup(){
+	
+	document.getElementById('race').addEventListener("change", setRace, false);
+	document.getElementById('STR').addEventListener("change", setSTRMod, false);
+	document.getElementById('DEX').addEventListener("change", setDEXMod, false);
+	document.getElementById('CON').addEventListener("change", setCONMod, false);
+	document.getElementById('INT').addEventListener("change", setINTMod, false);
+	document.getElementById('WIS').addEventListener("change", setWISMod, false);
+	document.getElementById('CHA').addEventListener("change", setCHAMod, false);
+}
+
 
 
 //Modifiers
-function setMod(value, id){
-	var mod = Math.floor(value / 2) - 5;
-	alert(mod);
-	document.getElementById(id +"_MOD").value = mod;
+
+function setSTRMod(){
+	setMod('STR');
+}
+
+function setDEXMod(){
+	setMod('DEX');
+}
+function setCONMod(){
+	setMod('CON');
+}
+function setINTMod(){
+	setMod('INT');
+}
+function setWISMod(){
+	setMod('WIS');
+}
+function setCHAMod(){
+	setMod('CHA');
+}
+
+function setMod(element){
+	
+	var ability = document.getElementById(element).value;
+	
+	if(!ability.match(numericExpression))
+	{
+		alert("Need a numeric input");
+		document.getElementById(element).value = 0;
+		return false;
+	}
+	
+	else if(ability > 18)
+	{
+		alert("Your input is too high for your character level");
+		document.getElementById(element).value = 0;
+		return false;
+	}
+	else if(ability < 3)
+	{
+		alert("Your input is too low for your character level");
+		document.getElementById(element).value = 0;
+		return false;
+	}		
+	else
+	{
+		var mod = Math.floor(ability / 2) - 5;
+		var i = 0;
+		while(document.getElementById(element+"_MOD"+i) != null)
+		{
+			document.getElementById(element+"_MOD"+i).value = mod;
+			i++;
+		}
+	}
+}
+
+function setRace(){
+	if(race == "HUMAN" || race == "DWARF" || race == "ELF" || race == "HALF-ELF" || race == "HALF-ORC"){
+		//size = medium
+		document.getElementById('size').value = "MEDIUM";
+	}
+	else if(race == "GNOME" || race == "HALFLING")
+	{
+		document.getElementById('size').value = "SMALL";
+	}
+	else
+	{
+		document.getElementById('size').value = "NO_SIZE";
+	}
 }
